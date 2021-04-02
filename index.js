7f664c5e172471877cbd4cac8c9962dea326198b
Shadid21 <76004861+Shadid21@users.noreply.github.com>
2021-04-02 17:34:31 +0600

    This is my first website. That i crated.

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = 0.90;
+        document.getElementById("result").innerHTML = "You Can Buy "+ Math.floor(num1 * num2) +" Dimond In This Ammount";
+}
